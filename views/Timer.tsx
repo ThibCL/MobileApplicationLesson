d0e167a06b1037db8d3f96f3d19ffd3a8d682bfa
TinyTinaa <lucasdj31@gmail.com>
2020-12-13 23:59:40 +0100

    Timer fonctionel mais ultra moche et temps fixe

--- views/Timer.tsx
+++ views/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useContext } from "react"
+import React, { FunctionComponent, useContext,useState, useEffect } from "react"
 import { View, Text, TouchableOpacity } from "react-native"
 import { StackNavigationProp } from "@react-navigation/stack"
 import { StackParamList } from "../App"
@@ -9,29 +9,73 @@ type ScreenNavigationProp = StackNavigationProp<StackParamList, "Timer">
 
 interface TimerProps {
    navigation: ScreenNavigationProp
-   this.state ={
-     time: 360,
-     isOn: false
-
-   }
+  
  }
 
  export const Timer: FunctionComponent<TimerProps> = ({
    navigation,
  }: TimerProps) => {
-   const StartTimer = () => {this.setState({
-    isOn: true})
-    setInterval(() => this.setState({
-      time: this.state.time-1
-    }), 1000);)
-   }
+  const [time, setTime] = useState(0)
+  const [isOn, setIsOn] = useState(false)
+  const [found, setFound] = useState(false)
+  const [timer, setTimer] = useState(300)
   
+
+  useEffect(() => {
+    if (isOn && time < timer) {
+      const interval = setInterval(() => {
+        setTime(time + 1)
+      }, 1000)    
+      return () => clearInterval(interval)
+      }
+    if (time==timer){
+      navigation.replace("Votes")
+    }
+  } )
 
    return (
      <View style={styles.container}>
-       <TouchableOpacity style={styles.buttonTouchable} onPress={() => StartTimer>
-        <Text style={styles.buttonText}>Start/Pause</Text>
+       { !found ? ( <Text style={styles.title}>Find the word !</Text>)
+       :( <Text style={styles.title}>Debate to find the impostor</Text>)}
+       <p>{Math.floor((timer-time)/60)}:{(timer-time)%60}</p>
+       
+       {isOn ? (
+         <TouchableOpacity
+         style={styles.buttonTouchable}
+         onPress={() => {
+           setIsOn(false)
+         }}
+       >
+         <Text style={styles.buttonText}>Pause</Text>
+       </TouchableOpacity>
+       )
+       :(
+        <TouchableOpacity
+        style={styles.buttonTouchable}
+        onPress={() => {
+          setIsOn(true)
+        }}
+      >
+        <Text style={styles.buttonText}>Start</Text>
       </TouchableOpacity>
+      
+       )
+       }
+      {!found ? (
+         <TouchableOpacity
+         style={styles.buttonTouchable}
+         onPress={() => {
+           setTimer(time)
+           setTime(0)
+           setFound(true)
+         }}
+       >
+         <Text style={styles.buttonText}>Word Found</Text>
+       </TouchableOpacity>
+      ) : null}
+
+       
+        
      </View>
    )
  }
